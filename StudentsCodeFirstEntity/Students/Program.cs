4c494f1f30182312350cdc600212f62e92e8025a
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-12-03 17:39:35 -0700

    Add files via upload

---
+++ StudentsCodeFirstEntity/Students/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentContext())
+            {
+                var student = new Student();
+                Console.WriteLine("What is the students first name?");
+                student.FirstName = Console.ReadLine();
+                Console.WriteLine("What is the students last name?");
+                student.LastName = Console.ReadLine();
+                Console.WriteLine("What is the students grade level?");
+                student.GradeLevel = Console.ReadLine();
+
+                context.Students.Add(student);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
